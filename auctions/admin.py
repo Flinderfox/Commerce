10497b16f08c25649a631241613eb7d859d121f3
Flinderfox <flinderfox@gmail.com>
2021-11-01 21:02:50 +0200

    Edit viewing Category

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import  User, Product, Auction, Watchlist, Bid, Chat
+from .models import  User, Category, Product, Auction, Watchlist, Bid, Chat
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Auction)
 admin.site.register(Watchlist)
